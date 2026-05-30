8ae073ae5ac58301944af34c38abdb0e9b327b6e
JolahMilovski <jolahmilovski@gmail.com>
2026-05-30 00:48:05 -0400

    start_in_oop

---
+++ python_oop/class.py
@@ -0,0 +1,15 @@
+class Number: 
+    def __init__(self, numb):
+        self.numb = numb
+
+numbr = Number(12)
+print("тип Number")
+print(type(Number))
+print("тип numb")
+print(type(numbr.numb))
+
+
+numbr =124
+print("изменили тип numbr присвоив в него 124")
+print(type(numbr))
+
